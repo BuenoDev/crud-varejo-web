e823b2d6d6d48889adcc23e453a936a0c35c2ef9
Bruno Moura <contato@brunosite.com>
2017-10-31 12:18:33 -0200

    Teste com Luíz

--- app/autoload.php
+++ app/autoload.php
@@ -1,6 +1,4 @@
 <?php
 /** Arquivo de inicialização de classes de forma automáticas já pré carregadas na memória */
 
-require __DIR__ . '\..\vendor\autoload.php';
-
-$controller = new \Varejo\Controller\Controller;
+require_once __DIR__ . '\..\vendor\autoload.php';
--- public/index.php
+++ public/index.php
@@ -1,21 +1,5 @@
 <?php
 /** Boot do sistema. */
-
 require_once __DIR__ . '\..\app\autoload.php';
 
-$users = new Varejo\Model\User;
-
-$dados = [
-    'name' => 'Joãozinho Pé de Chinelo',
-    'email' => 'ze@brunosite.com',
-    'password' => sha1('123456'),
-    'role' => 'client'
-];
-
-$users->update($dados, ['id', '=', '2']);
-
-// $users->insert($dados);
-// if($users){
-    
-// }
-
+$controller = new \Varejo\Controller\Controller;
