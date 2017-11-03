62514efdfbb04a182b3882a07c09b5789ea69791
Lupe <lfbmrj15@gmail.com>
2017-11-03 13:50:45 -0200

    Design pagina de login

--- app/route.php
+++ app/route.php
@@ -6,7 +6,7 @@
 //     ]
 // ];
 $route->get('/', 'Home@index');
-$route->get('/test', 'Test@show');
+$route->get('/teste', 'Test@show');
 // $this->input=[
 //     "POST"=>[
 //         "/login/auth" => "Login@auth",
