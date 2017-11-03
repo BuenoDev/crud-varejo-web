02644d758a9b7fac56fa5bab0a582756a40dbc9a
Bruno Moura <contato@brunosite.com>
2017-11-03 17:02:30 -0200

    Classe Update register to data bases

--- app/Conn/Update.php
+++ app/Conn/Update.php
@@ -17,46 +17,67 @@ class Update extends Conn{
     /**
      * Iniciamos uma atuaização de registro no banco de dados de acordo com a tabela
      */
-    public function execute($table, array $data, $clause = null, $parce_string = null){
+    public function execute($table, array $data, $clause, $parce_string){
         $this->table = (string) $table;
         $this->data = $data;
         $this->clause = (string) $clause;
+        parse_str($parce_string, $this->parce_string);
 
         $this->buildQuery();
+        $this->finalize();
     }
 
+    /**
+     * Retorna o resultada da uatualização de registro no banco de dados
+     * @return boolean
+     */
     public function result(){
         return $this->result;
     }
 
+    /**
+     * Retorn o quantidades de registros atualizados no banco
+     * @return int
+     */
     public function countUpdated(){
-
+        return $this->query->rowCount();
     }
+
+    /**
+     * Contrução da query SQL seguindo o padrão PDO Statements
+     */
     public function buildQuery(){
         $data = [];
         foreach($this->data as $key => $value){
-            $data[] = $key . ' = ' . $value;
+            $data[] = $key . ' = :' . $key;
         }
-        $set = implde(', ', $data);
+        $set = implode(', ', $data);
 
-        dd($set);
-
-        /** Padrão SQL
-         * UPDATE table_name SET col1 = val1, col2 = val2 WHERE field = value 
-         */
-        
-        /** 
-         * Padrão PDO
-         *  UPDATE table_name SET col1 = :col1, col2 = :col2 WHERE field = :field 
-         */
-
+        /** Construção da Query segundo o padrão PDO Statements */
+        $this->query = "UPDATE {$this->table} SET {$set} {$this->clause}";
     }
 
+    /**
+     * Prepara a query para receber os valores dos dados
+     */
     public function prepareQuery(){
-
+        $this->conn = parent::getConn();
+        $this->query = $this->conn->prepare($this->query);
     }
 
+    /**
+     * Finaliza a atualização executando a query no banco de dados
+     */
     public function finalize(){
+        $this->prepareQuery();
+        try{
+            /** Unimos os 2 arrays para que PDO possa trocar as variáveis correspondentes por valores respectivos */
+            $this->query->execute(array_merge($this->data, $this->parce_string));
 
+            $this->result = true;
+        } catch (\PDOException $e){
+            error_debug($e, "Atualização de dado não realizada");
+            $this->result = null;
+        }
     }
 }
