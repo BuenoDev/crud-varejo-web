ba0dfe6c0a99b19c23f790604d930ba03fb97f8c
Lupe <lfbmrj15@gmail.com>
2017-11-09 11:15:27 -0200

    editado dados.css da pagina produtos

--- resources/views/pieces/dados/produtos.view.php
+++ resources/views/pieces/dados/produtos.view.php
@@ -5,99 +5,19 @@
             <th>Codigo</th>
             <th>Valor</th>
             <th>Quantidade</th>
-            <th>Atualizado em </th>
             <th colspan="3">Ação</th>
         </tr>
+        <?php for($i=0;$i<100;$i++):  ?>
         <tr>
             <td>umo</td>
             <td>buga</td>
             <td>tedi</td>
             <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
+            <td><a href=""><i class="fa fa-opencart"></i></a></td>
+            <td><a href=""><i class="fa fa-edit"></i></a></td>
+            <td><a href=""><i class="fa fa-trash"></i></a></td>
         </tr>
-        <tr>
-        <td>umo</td>
-        <td>buga</td>
-        <td>tedi</td>
-        <td>tal</td>
-        <td>loop</td>
-        <td><i class="fa fa-opencart"></i></td>
-        <td><i class="fa fa-edit"></i></td>
-        <td><i class="fa fa-trash"></i></td>
-    </tr>
-    <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
-        <tr>
-            <td>umo</td>
-            <td>buga</td>
-            <td>tedi</td>
-            <td>tal</td>
-            <td>loop</td>
-            <td><i class="fa fa-opencart"></i></td>
-            <td><i class="fa fa-edit"></i></td>
-            <td><i class="fa fa-trash"></i></td>
-        </tr>
+        <?php endfor ; ?>
 
     </table>
 </div>
