12ac307db40add5152425e6a961d98d0d49eef73
root <root@DESKTOP-OBRM5V9.localdomain>
2017-11-15 02:20:03 +0000

    Classe de deleção de conteúdo

--- app/Services/Conn/Delete.php
+++ app/Services/Conn/Delete.php
@@ -1,7 +1,58 @@
 <?php
-/** Conexão com banco de dados usando padrão SingleTon */
+/**
+ * Classe responsável por deletar genéricamente informações no banco de dados
+ * @author Bruno Moura <contato@brunosite.com>
+ */
+
 namespace Varejo\Services\Conn;
 
-class Delete{
-    
+class Delete extends Conn {
+
+    private $table;
+    private $clause;
+    private $parse_string;
+    private $result;
+
+    private $query;
+
+    private $conn;
+
+    public function execute($table, $clause, $parse_string) {
+        $this->table = (string) $table;
+        $this->clause = (string) $clause;
+
+        parse_str($parse_string, $this->parse_string);
+
+        $this->buildQuery();
+        $this->finalize();
+        
+    }
+
+    public function result() {
+        return $this->result;
+    }
+
+    public function count() {
+        return $this->query->rowCount();
+    }
+
+    private function buildQuery() {
+        $this->query = "DELETE FROM {$this->table} {$this->clause}";
+
+        $this->conn = parent::getConn();
+        $this->query = $this->conn->prepare($this->query);
+    }
+
+    private function finalize() {
+        $this->buildQuery();
+        try {
+            $this->query->execute($this->parse_string);
+            $this->result = true;
+        } catch (PDOException $e) {
+            $this->result = NULL;
+            error_debug($e, '<b>Erro ao Deletar conteúdo: </b>');
+            die;
+        }
+    }
+
 }
